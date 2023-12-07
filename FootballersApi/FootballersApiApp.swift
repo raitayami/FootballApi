15526f9d9f71cca18e3efe4f285367d5b13ee45a
raitayami <66219215+raitayami@users.noreply.github.com>
2023-12-07 17:56:29 +0000

    Initial Commit

---
+++ FootballersApi/FootballersApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballersApiApp.swift
+//  FootballersApi
+//
+//  Created by Tayami Rai on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FootballersApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
